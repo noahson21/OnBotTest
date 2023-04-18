e24541251944ab5fbe102546127311f1549ccb5e
noahson21 <90984999+noahson21@users.noreply.github.com>
2023-04-18 15:43:53 -0700

    Add files via upload

---
+++ TeleOp.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+
+public class TeleOp {
+
+    // todo: write your code here
+    
+    
+}
